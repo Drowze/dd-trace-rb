fc53eab6a19716e2af0ff55c98d64b9a5e657d7e
Rafael Gibim <9031589+Drowze@users.noreply.github.com>
2025-12-08 15:55:26 -0300

    Apply karafka/waterdrop changes on v2.22.0
    Simply:
    - git reset --hard v2.22.0
    - git checkout karafka-configuration-per-topic -- \
      lib/datadog/tracing/contrib/{karafka,waterdrop} \
      spec/datadog/tracing/contrib/{karafka,waterdrop} \
      lib/datadog/tracing/contrib.rb \
      lib/datadog/tracing/contrib/waterdrop.rb

--- lib/datadog/tracing/contrib/karafka/patcher.rb
+++ lib/datadog/tracing/contrib/karafka/patcher.rb
@@ -10,14 +10,6 @@ module Datadog
       module Karafka
         # Patch to add tracing to Karafka::Messages::Messages
         module MessagesPatch
-          def configuration
-            Datadog.configuration.tracing[:karafka]
-          end
-
-          def propagation
-            @propagation ||= Contrib::Karafka::Distributed::Propagation.new
-          end
-
           # `each` is the most popular access point to Karafka messages,
           # but not the only one
           #  Other access patterns do not have a straightforward tracing avenue
@@ -25,17 +17,35 @@ module Datadog
           # @see https://github.com/karafka/karafka/blob/b06d1f7c17818e1605f80c2bb573454a33376b40/README.md?plain=1#L29-L35
           def each(&block)
             @messages_array.each do |message|
-              if configuration[:distributed_tracing]
+              configuration = datadog_configuration(message.topic)
+              trace_digest = if configuration[:distributed_tracing]
                 headers = if message.metadata.respond_to?(:raw_headers)
                   message.metadata.raw_headers
                 else
                   message.metadata.headers
                 end
-                trace_digest = Karafka.extract(headers)
-                Datadog::Tracing.continue_trace!(trace_digest) if trace_digest
+                Karafka.extract(headers)
               end
 
-              Tracing.trace(Ext::SPAN_MESSAGE_CONSUME) do |span|
+              Tracing.trace(Ext::SPAN_MESSAGE_CONSUME, continue_from: trace_digest) do |span, trace|
+                if Datadog::DataStreams.enabled?
+                  begin
+                    headers = if message.metadata.respond_to?(:raw_headers)
+                      message.metadata.raw_headers
+                    else
+                      message.metadata.headers
+                    end
+
+                    Datadog::DataStreams.set_consume_checkpoint(
+                      type: 'kafka',
+                      source: message.topic,
+                      auto_instrumentation: true
+                    ) { |key| headers[key] }
+                  rescue => e
+                    Datadog.logger.debug("Error setting DSM checkpoint: #{e.class}: #{e}")
+                  end
+                end
+
                 span.set_tag(Ext::TAG_OFFSET, message.metadata.offset)
                 span.set_tag(Contrib::Ext::Messaging::TAG_DESTINATION, message.topic)
                 span.set_tag(Contrib::Ext::Messaging::TAG_SYSTEM, Ext::TAG_SYSTEM)
@@ -46,11 +56,19 @@ module Datadog
               end
             end
           end
+
+          private
+
+          def datadog_configuration(topic)
+            Datadog.configuration.tracing[:karafka, topic]
+          end
         end
 
         # Patcher enables patching of 'karafka' module.
         module Patcher
           include Contrib::Patcher
+
+          ACTIVATE_FRAMEWORK_ONLY_ONCE = Core::Utils::OnlyOnce.new
 
           module_function
 
@@ -60,9 +78,21 @@ module Datadog
 
           def patch
             require_relative 'monitor'
+            require_relative 'framework'
+            require_relative '../waterdrop'
 
             ::Karafka::Instrumentation::Monitor.prepend(Monitor)
             ::Karafka::Messages::Messages.prepend(MessagesPatch)
+
+            if Contrib::WaterDrop::Integration.compatible?
+              ::Karafka.monitor.subscribe('app.initialized') do |event|
+                ACTIVATE_FRAMEWORK_ONLY_ONCE.run do
+                  Contrib::Karafka::Framework.setup
+                end
+
+                Contrib::WaterDrop::Patcher.add_middleware(::Karafka.producer)
+              end
+            end
           end
         end
       end
